0dfeaadbe024c24da8790ea26902aaeb4bcdd71e
fgaviriaisaza <f.gaviria@uniandes.edu.co>
2025-01-31 14:17:51 -0500

    +Apellido2 y correcion nombre

--- Archivo Python.py
+++ Archivo Python.py
@@ -1,5 +1,6 @@
 texto1="Fel"
 texto2="ipe"
 Apellido= "Gaviria"
-NombreC=texto1 + " " + texto2 + " " + Apellido
+Apellido2="Isaza"
+NombreC=texto1 + texto2 + " " + Apellido + Apellido2
 print(NombreC)
